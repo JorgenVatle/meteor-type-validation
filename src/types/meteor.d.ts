56b5d4870f3d354a8fa746da7afaad5e9be6c19b
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-07 18:51:47 +0100

    Remove redundant exports

--- src/types/meteor.d.ts
+++ src/types/meteor.d.ts
@@ -1,17 +1,17 @@
 declare module 'meteor/meteor' {
     // These are left empty so you can merge in your own types
-    export interface DefinedMethods {}
-    export interface DefinedPublications {}
+    interface DefinedMethods {}
+    interface DefinedPublications {}
     
     module Meteor {
-        export function subscribe<
+        function subscribe<
             TName extends keyof DefinedPublications
         >(
             name: TName,
             ...params: Parameters<DefinedPublications[TName]>
         ): Meteor.SubscriptionHandle;
         
-        export function call<TName extends keyof DefinedMethods>(
+        function call<TName extends keyof DefinedMethods>(
             name: TName,
             ...params: [
                 ...Parameters<DefinedMethods[TName]>,
@@ -19,7 +19,7 @@ declare module 'meteor/meteor' {
             ],
         ): void;
         
-        export function callAsync<TName extends keyof DefinedMethods>(
+        function callAsync<TName extends keyof DefinedMethods>(
             name: TName,
             ...params: Parameters<DefinedMethods[TName]>
         ): Awaited<ReturnType<DefinedMethods[TName]>>;
