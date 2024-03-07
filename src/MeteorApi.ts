92b400f1c003ac56f39c27a49442d2f48de08818
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-07 19:22:10 +0100

    Add call() method

--- src/MeteorApi.ts
+++ src/MeteorApi.ts
@@ -2,11 +2,23 @@ import { Meteor, DefinedMethods, DefinedPublications } from 'meteor/meteor';
 
 export default new class MeteorApi {
     
-    public callAsync<TMethod extends keyof DefinedMethods>(
-        name: TMethod,
-        ...params: Parameters<DefinedMethods[TMethod]>
+    public callAsync<TName extends keyof DefinedMethods>(
+        name: TName,
+        ...params: Parameters<DefinedMethods[TName]>
     ) {
-        return Meteor.callAsync<TMethod>(name, ...params);
+        return Meteor.callAsync<TName>(name, ...params);
     }
+    
+    public call<TName extends keyof DefinedMethods>(
+        name: TName,
+        ...params: [
+            ...Parameters<DefinedMethods[TName]>,
+            ...[callback?: (error?: Error, response?: ReturnType<DefinedMethods[TName]>) => void]
+        ]
+    ) {
+        return Meteor.call<TName>(name, ...params);
+    }
+    
+   
     
 }
