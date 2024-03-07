a32c59c1dbdf504317a43ecce602e6ed24157b4c
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-07 19:26:37 +0100

    Simplify MeteorApi exports

--- src/MeteorApi.ts
+++ src/MeteorApi.ts
@@ -1,26 +1,10 @@
 import { Meteor, DefinedMethods, DefinedPublications } from 'meteor/meteor';
 
-export default new class MeteorApi {
-    
-    public callAsync<TName extends keyof DefinedMethods>(
-        name: TName,
-        ...params: Parameters<DefinedMethods[TName]>
-    ) {
-        return Meteor.callAsync<TName>(name, ...params);
-    }
-    
-    public call<TName extends keyof DefinedMethods>(
-        name: TName,
-        ...params: [
-            ...Parameters<DefinedMethods[TName]>,
-            ...[callback?: (error?: Error, response?: ReturnType<DefinedMethods[TName]>) => void]
-        ]
-    ) {
-        return Meteor.call<TName>(name, ...params);
-    }
-    
-    public subscribe<TName extends keyof DefinedPublications>(name: TName, ...params: Parameters<DefinedPublications[TName]>) {
-        return Meteor.subscribe<TName>(name, ...params);
-    }
-    
+/**
+ * Overrides for Meteor's default types to enforce type safety.
+ */
+export const MeteorApi = {
+    callAsync: Meteor.callAsync<keyof DefinedMethods>,
+    call: Meteor.call<keyof DefinedMethods>,
+    subscribe: Meteor.subscribe<keyof DefinedPublications>,
 }
