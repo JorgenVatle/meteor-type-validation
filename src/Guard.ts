9768f0cc57d69e08fa727c55d8f1a9f9325a10fc
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-07 13:37:31 +0100

    Add Guard class abstract

---
+++ src/Guard.ts
@@ -0,0 +1,16 @@
+import { Meteor, Subscription } from 'meteor/meteor';
+
+export abstract class Guard {
+    constructor(
+        public readonly context: GuardInputContext,
+        protected readonly params: unknown[]
+    ) {}
+    public abstract validate(): asserts this;
+    public abstract get validatedContext(): unknown;
+}
+
+export type GuardInputContext = Meteor.MethodThisType | Subscription
+
+export interface GuardStatic<TGuard extends Guard = Guard> {
+    new(...context: any): TGuard;
+}
