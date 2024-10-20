29f58d5ea1022c558943b8808145e363aa815aac
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-20 20:31:42 +0300

    Fix type error for globally accessible Meteor property

--- src/globals/Meteor.ts
+++ src/globals/Meteor.ts
@@ -1,5 +1,6 @@
 // Doing a little bit of a hack here so we won't have to depend on an Atmosphere package
 import type { Meteor as _Meteor } from 'meteor/meteor';
 export type { Subscription, DefinedMethods, DefinedPublications } from 'meteor/meteor';
+declare const Meteor: unknown;
 const MeteorGlobal = Meteor as typeof _Meteor;
 export { MeteorGlobal as Meteor };
