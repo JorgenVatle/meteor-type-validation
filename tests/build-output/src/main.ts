bc92c5428becc30e60bdd6090c4e02c6e05fda71
Jørgen Vatle <Jorgen@WcAServices.net>
2025-02-21 04:47:04 +0200

    Export instead of augment defined api resources

--- tests/build-output/src/main.ts
+++ tests/build-output/src/main.ts
@@ -1,7 +1,5 @@
 import { UnwrapMethods, UnwrapPublications } from 'meteor-type-validation';
 import type { Methods, Publications } from './api';
 
-declare module 'meteor/meteor' {
-    interface DefinedPublications extends UnwrapPublications<typeof Publications> {}
-    interface DefinedMethods extends UnwrapMethods<typeof Methods> {}
-}
+export interface DefinedPublications extends UnwrapPublications<typeof Publications> {}
+export interface DefinedMethods extends UnwrapMethods<typeof Methods> {}
