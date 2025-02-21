6de176e08ca4ba068234aaca5bf466f9813bba2f
Jørgen Vatle <Jorgen@WcAServices.net>
2025-02-21 03:44:38 +0200

    Add mock API

---
+++ tests/build-output/api.ts
@@ -0,0 +1,27 @@
+import { defineMethods, definePublications, UnwrapPublications, UnwrapMethods } from 'meteor-type-validation';
+import { Mongo } from 'meteor/mongo';
+import { type Order, OrderSchema } from './schemas';
+
+export const Methods = defineMethods({
+    'orders.create': {
+        schema: [OrderSchema],
+        guards: [],
+        method(order) {
+            return  order;
+        }
+    }
+});
+
+
+export const Publications = definePublications({
+    'orders': {
+        schema: [],
+        guards: [],
+        publish() {
+            return {} as Mongo.Cursor<Order>
+        }
+    }
+});
+
+export interface DefinedPublications extends UnwrapPublications<typeof Publications> {}
+export interface DefinedMethods extends UnwrapMethods<typeof Methods> {}
