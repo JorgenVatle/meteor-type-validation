4a6dd1f5db73e6cbf786947c6c2d6510f82d0714
Jørgen Vatle <Jorgen@WcAServices.net>
2025-02-21 04:32:34 +0200

    Correct meteor-type-validation import path

--- tests/build-output/src/api.ts
+++ tests/build-output/src/api.ts
@@ -1,4 +1,4 @@
-import { defineMethods, definePublications, UnwrapPublications, UnwrapMethods } from 'dist/index';
+import { defineMethods, definePublications, UnwrapPublications, UnwrapMethods } from 'meteor-type-validation';
 import { Mongo } from 'meteor/mongo';
 import { type Order, OrderSchema } from './schemas';
 
