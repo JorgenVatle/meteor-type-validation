b87db8315e58ebbe2d8f91e9d932138957ed8a16
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 22:26:57 +0200

    Implement validation in UserAuthenticated guard

--- src/guards/UserAuthenticated.ts
+++ src/guards/UserAuthenticated.ts
@@ -1,7 +1,12 @@
+import { Meteor } from '@meteor';
 import { Guard } from './Guard';
 
 export class UserAuthenticated extends Guard {
-    public validate(): asserts this is { context: { userId: string } } {}
+    public validate(): asserts this is { context: { userId: string } } {
+        if (!this.context.userId) {
+            throw new Meteor.Error(401, 'You need to be logged in first');
+        }
+    }
     public get validatedContext() {
         this.validate();
         return this.context;
