738be0e641a3ff44465862c8a9c82600fd150deb
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 19:02:18 +0200

    Add context test for publications

--- tests/type-tests/guards/UserAuthenticated.test-d.ts
+++ tests/type-tests/guards/UserAuthenticated.test-d.ts
@@ -1,6 +1,6 @@
 import * as v from 'valibot';
-import { expectTypeOf, it } from 'vitest';
-import { defineMethods } from '../../../src/Definitions';
+import { describe, expectTypeOf, it } from 'vitest';
+import { defineMethods, definePublications } from '../../../src/Definitions';
 import { UserAuthenticated } from '../../../src/guards/UserAuthenticated';
 
 const TodoDocumentSchema = v.object({
@@ -8,16 +8,34 @@ const TodoDocumentSchema = v.object({
     completed: v.boolean(),
 })
 
-it(`should infer that this.userId is not null`, () => {
-    defineMethods({
-        'todos.create': {
-            guards: [UserAuthenticated],
-            schema: [
-                TodoDocumentSchema,
-            ],
-            method(todo) {
-                expectTypeOf(this.userId).toEqualTypeOf<string>();
+describe('methods', () => {
+    it(`should infer that this.userId is not null`, () => {
+        defineMethods({
+            'todos.create': {
+                guards: [UserAuthenticated],
+                schema: [
+                    TodoDocumentSchema,
+                ],
+                method(todo) {
+                    expectTypeOf(this.userId).toEqualTypeOf<string>();
+                }
             }
-        }
+        })
+    })
+})
+
+describe('publications', () => {
+    it(`should infer that this.userId is not null`, () => {
+        definePublications({
+            'todos': {
+                guards: [UserAuthenticated],
+                schema: [
+                    TodoDocumentSchema,
+                ],
+                publish(todo) {
+                    expectTypeOf(this.userId).toEqualTypeOf<string>();
+                }
+            }
+        })
     })
 })
