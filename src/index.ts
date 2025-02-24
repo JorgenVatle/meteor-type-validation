9ecc689d53e4655316629fb93ac9de7098062fd7
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 20:56:04 +0200

    Add UserAuthenticated guard to package exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './types/meteor/ModuleAugmentation';
 
 export * from './Definitions';
 export * from './guards/Guard';
+export * from './guards/UserAuthenticated';
 export * from './MeteorTypeValidation';
 
 export type * from './types/ValidatedResources';
