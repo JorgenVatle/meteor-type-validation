7cf4b8fb2f83e25e960b64b0ac6e880d473f65f0
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 01:14:00 +0200

    Update CreatedByCurrentUserGuard to use new guard validation strategy

--- tests/lib/CreatedByCurrentUserGuard.ts
+++ tests/lib/CreatedByCurrentUserGuard.ts
@@ -1,23 +1,22 @@
-import type { WrappedContext } from '../../src';
+import { Meteor } from '@meteor';
+import * as v from 'valibot';
 import { UserAuthenticated } from '../../src/guards/UserAuthenticated';
 
 export class CreatedByCurrentUser extends UserAuthenticated {
-    constructor(context: WrappedContext, protected readonly params: [{ createdBy: string }, ...rest: unknown[]]) {
-        super(context, params);
-    }
+    public readonly writeToParams = false;
+    public readonly writeToContext = false;
     
-    public validate(): asserts this is {
-        context: { userId: string },
-        params: [{ createdBy: string }, ...unknown[]]
-    } {
-        if (!this.params[0]?.createdBy) {
-            throw new Error('No user ID')
+    
+    public readonly inputSchema = [
+        v.object({
+            createdBy: v.string(),
+        }),
+    ]
+    
+    public validate() {
+        this.assertContext();
+        if (this.params[0].createdBy !== this.context.userId) {
+            throw new Meteor.Error(401, 'You do not have permission for this resource');
         }
-        return super.validate();
-    }
-    
-    public get validatedContext() {
-        this.validate();
-        return this.context;
     }
 }
