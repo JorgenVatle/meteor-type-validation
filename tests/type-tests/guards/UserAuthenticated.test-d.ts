189616b0d7d4f108fbd738ff1592d203cd5dfe7e
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 18:58:23 +0200

    Simplify test testing

--- tests/type-tests/guards/UserAuthenticated.test-d.ts
+++ tests/type-tests/guards/UserAuthenticated.test-d.ts
@@ -1,5 +1,5 @@
 import * as v from 'valibot';
-import { describe, expectTypeOf, it } from 'vitest';
+import { expectTypeOf, it } from 'vitest';
 import { defineMethods } from '../../../src/Definitions';
 import { UserAuthenticated } from '../../../src/guards/UserAuthenticated';
 
@@ -8,21 +8,16 @@ const TodoDocumentSchema = v.object({
     completed: v.boolean(),
 })
 
-describe('Method guard context type inference', () => {
-    describe('UserAuthenticated guard', () => {
-        it(`should infer that this.userId is not null`, () => {
-            defineMethods({
-                'todos.create': {
-                    guards: [UserAuthenticated],
-                    schema: [
-                        TodoDocumentSchema,
-                    ],
-                    method(todo) {
-                        expectTypeOf(this.userId).toEqualTypeOf<string>();
-                    }
-                }
-            })
-        })
+it(`should infer that this.userId is not null`, () => {
+    defineMethods({
+        'todos.create': {
+            guards: [UserAuthenticated],
+            schema: [
+                TodoDocumentSchema,
+            ],
+            method(todo) {
+                expectTypeOf(this.userId).toEqualTypeOf<string>();
+            }
+        }
     })
-
 })
