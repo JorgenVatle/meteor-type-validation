a37989abdc6e6f2768c12311f29065b6a500c17f
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 21:57:50 +0200

    Add admin guard

---
+++ tests/lib/AdminGuard.ts
@@ -0,0 +1,33 @@
+import { Meteor } from '@meteor';
+import type { InferOutput } from 'valibot';
+import * as v from 'valibot';
+import { UserAuthenticated } from '../../src/guards/UserAuthenticated';
+
+export class CreatedByCurrentUser extends UserAuthenticated {
+    
+    public validate(): asserts this is {
+        context: InferOutput<CreatedByCurrentUser['contextSchema']>,
+    } {
+        return super.validate();
+    }
+    
+    public readonly contextSchema = v.objectAsync({
+        userId: v.pipeAsync(v.string(), v.checkAsync(async (userId) => {
+            const user = await Meteor.users.findOneAsync({ _id: userId });
+            return !!user?.roles.includes('admin');
+        })),
+    })
+    
+    public get validatedContext() {
+        this.validate();
+        return this.context;
+    }
+}
+
+declare module 'meteor/meteor' {
+    namespace Meteor {
+        interface User {
+            roles: ('admin' | 'user')[];
+        }
+    }
+}
