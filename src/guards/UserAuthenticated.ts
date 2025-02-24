9a234a1ad6d381fa50893feaa1c91a2b69f9cfda
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 23:26:26 +0200

    Use context schema for validating login status

--- src/guards/UserAuthenticated.ts
+++ src/guards/UserAuthenticated.ts
@@ -1,14 +1,18 @@
 import { Meteor } from '@meteor';
+import * as v from 'valibot';
 import { Guard } from './Guard';
 
 export class UserAuthenticated extends Guard {
-    public validate(): asserts this is { context: { userId: string } } {
-        if (!this.context.userId) {
-            throw new Meteor.Error(401, 'You need to be logged in first');
-        }
-    }
-    public get validatedContext() {
-        this.validate();
-        return this.context;
-    }
+    
+    public readonly contextSchema = v.object({
+        userId: v.pipe(
+            v.nullish(v.string()),
+            v.transform((userId): string => {
+                if (!userId) {
+                    throw new Meteor.Error(401, 'You need to be logged in first');
+                }
+                return userId;
+            }),
+        ),
+    })
 }
