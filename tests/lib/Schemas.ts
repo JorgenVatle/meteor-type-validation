cbacbe5af1410daa95a84ec3f5c9c9688ba5ed17
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-24 19:12:31 +0200

    Add edit todo schema

--- tests/lib/Schemas.ts
+++ tests/lib/Schemas.ts
@@ -4,3 +4,8 @@ export const CreateTodoSchema = v.object({
     title: v.string(),
     completed: v.boolean(),
 });
+
+export const EditTodoSchema = v.object({
+    _id: v.string(),
+    ...CreateTodoSchema.entries,
+})
