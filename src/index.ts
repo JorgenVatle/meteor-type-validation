e61e7c4e69f589d090e0e784d663bf60ff9985b6
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 15:01:12 +0200

    Reformat codestyle

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './types/meteor/ModuleAugmentation';
 
-export * from './Definitions';
 export * from './guards';
+export * from './Definitions';
 export * from './MeteorTypeValidation';
 
 export type * from './types/ValidatedResources';
