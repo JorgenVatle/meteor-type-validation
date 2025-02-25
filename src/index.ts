dc5a4a9aa54306db026b605424f396f175c4e7c6
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 15:00:50 +0200

    Remove redundant re-export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import './types/meteor/ModuleAugmentation';
 
 export * from './Definitions';
 export * from './guards';
-export * from './guards/UserAuthenticated';
 export * from './MeteorTypeValidation';
 
 export type * from './types/ValidatedResources';
