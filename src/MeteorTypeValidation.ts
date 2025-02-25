43b11f730003e27d87f73544a04ce8aaf9c896fa
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 17:51:39 +0200

    Revert "Handle type error from Meteor.wrapAsync"
    This reverts commit 8b0a8203f8a8c70ebd87a091e55f4c4d19638fbe.

--- src/MeteorTypeValidation.ts
+++ src/MeteorTypeValidation.ts
@@ -167,10 +167,8 @@ export class MeteorTypeValidation<
         }
     }
     
-    protected withErrorHandler(method: (...params: unknown[]) => unknown): ((...params: unknown[]) => any) {
+    protected withErrorHandler(method: (...params: unknown[]) => unknown): ((...params: unknown[]) => any) | Function {
         const customErrorHandler = this.options.errorHandler?.bind(this);
-        
-        // @ts-expect-error (Bad typing from @types/meteor)
         return Meteor.wrapAsync(async function(this: WrappedContext & TExtendedContext, ...inputParams: unknown[]) {
             const params = inputParams.slice(0, -1);
             try {
