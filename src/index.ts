be9d226ea546635298202e3bbb18d86bf354ed6c
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 15:00:05 +0200

    Add re-export for guards

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './types/meteor/ModuleAugmentation';
 
 export * from './Definitions';
-export * from './guards/Guard';
+export * from './guards';
 export * from './guards/UserAuthenticated';
 export * from './MeteorTypeValidation';
 
