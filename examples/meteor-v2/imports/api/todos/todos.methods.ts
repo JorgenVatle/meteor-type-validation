0eb472e56290b0236947ce9e2f3a956f1db8c539
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-28 18:49:05 +0200

    Add todos delete method

--- examples/meteor-v2/imports/api/todos/todos.methods.ts
+++ examples/meteor-v2/imports/api/todos/todos.methods.ts
@@ -1,4 +1,5 @@
 import { defineMethods } from 'meteor-type-validation';
+import * as v from 'valibot';
 import { TodosCollection } from './todos.collection';
 import { TodosSchema } from './todos.schema';
 
@@ -15,6 +16,13 @@ export default defineMethods({
         guards: [],
         method(todo) {
             TodosCollection.update(todo._id, { $set: todo });
+        }
+    },
+    'todos.delete': {
+        schema: [v.pick(TodosSchema, ['_id'])],
+        guards: [],
+        method({ _id }) {
+            TodosCollection.remove(_id);
         }
     }
 })
