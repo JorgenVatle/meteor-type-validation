098fd6add4a5dc75b3f30fcf17046e79076f1f00
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-28 18:59:12 +0200

    Add BelongsToCurrentUser guard

---
+++ examples/meteor-v2/imports/lib/guards/BelongsToCurrentUser.ts
@@ -0,0 +1,16 @@
+import { Guard, UserAuthenticated } from 'meteor-type-validation';
+import * as v from 'valibot';
+
+export class BelongsToCurrentUser extends Guard {
+    public readonly writeToContext = false;
+    public readonly writeToParams = true;
+    
+    public readonly contextSchema = UserAuthenticated.contextSchema;
+    public readonly inputSchema = [
+        v.pipe(
+            v.looseObject({}),
+            v.transform((input) => Object.assign(input, { userId: this.context.userId }))
+        )
+    ]
+    
+}
