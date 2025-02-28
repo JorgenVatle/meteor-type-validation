24cd866c6964464163bce9369b1b05c3dca198ca
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-28 18:22:07 +0200

    Correct path to tsup entry

--- examples/meteor-v2/tsup.config.ts
+++ examples/meteor-v2/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     target: 'node14',
-    entry: ['./server/main.ts'],
+    entry: ['./server/entry-tsup.ts'],
     outDir: 'server/_bundle',
     skipNodeModulesBundle: true,
     sourcemap: true,
