71036fd15cf944cb5390ccbe0b657a1b49568177
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-28 21:25:37 +0200

    Unmark todo test now that an implementation is in place

--- packages/meteor-type-validation/tests/type-tests/guards/CustomGuards.test-d.ts
+++ packages/meteor-type-validation/tests/type-tests/guards/CustomGuards.test-d.ts
@@ -18,13 +18,12 @@ describe('CreatedByCurrentUser', () => {
             })
         });
         
-        it.todo('should extend the input params with a createdBy selector', () => {
+        it('should extend the input params with a createdBy selector', () => {
             defineMethods({
                 'todo.edit': {
                     schema: [EditTodoSchema],
                     guards: [CreatedByCurrentUser],
                     method(entry) {
-                        // @ts-expect-error (This still needs to be implemented)
                         expectTypeOf(entry).toMatchTypeOf<{ createdBy: string }>()
                     }
                 }
