4be4178800cc8daff408d41ac2f0a8942d0925f9
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-28 19:20:54 +0200

    Add belongs to current user guard to methods

--- examples/meteor-v2/imports/api/todos/todos.methods.ts
+++ examples/meteor-v2/imports/api/todos/todos.methods.ts
@@ -1,25 +1,26 @@
 import { defineMethods } from 'meteor-type-validation';
+import { BelongsToCurrentUser } from '../../lib/guards/BelongsToCurrentUser';
 import { TodosCollection } from './todos.collection';
 import { TodoCreateSchema, TodoEditSchema, TodosSelector } from './todos.schema';
 
 export default defineMethods({
     'todos.create': {
         schema: [TodoCreateSchema],
-        guards: [],
+        guards: [BelongsToCurrentUser],
         method(todo) {
             TodosCollection.insert(todo);
         }
     },
     'todos.edit': {
         schema: [TodosSelector, TodoEditSchema],
-        guards: [],
+        guards: [BelongsToCurrentUser],
         method(selector, todo) {
             TodosCollection.update(selector, { $set: todo });
         }
     },
     'todos.delete': {
         schema: [TodosSelector],
-        guards: [],
+        guards: [BelongsToCurrentUser],
         method(selector) {
             TodosCollection.remove(selector);
         }
