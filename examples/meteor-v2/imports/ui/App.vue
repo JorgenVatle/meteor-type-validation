ced74d5b8f67dd9247808386c8c225ec50b10395
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 05:10:36 +0200

    Add user authentication flow to app

--- examples/meteor-v2/imports/ui/App.vue
+++ examples/meteor-v2/imports/ui/App.vue
@@ -9,6 +9,22 @@
       <div>
         <button @click="count++" class="btn">This button has been clicked {{ count }} times</button>
       </div>
+
+      <div>
+        <h1 class="text-2xl">Todos</h1>
+      </div>
+      <div>
+        <div v-if="!user.loggedIn">
+          <p>You need to log in before you can view todos</p>
+          <button @click="user.login()" class="btn">Login</button>
+        </div>
+        <div v-else-if="user.loading">
+          <p>Waiting on login...</p>
+        </div>
+        <div>
+          <p>Todos should appear here</p>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -16,6 +32,8 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useCurrentUser } from './composition/users/useCurrentUser';
 
 const count = ref(0);
+const user = useCurrentUser();
 </script>
