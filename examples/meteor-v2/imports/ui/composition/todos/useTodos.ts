705a63eb73b2bca03f4c2be4171b5e300d67a3e4
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 05:47:08 +0200

    Add create() method to todos composable

--- examples/meteor-v2/imports/ui/composition/todos/useTodos.ts
+++ examples/meteor-v2/imports/ui/composition/todos/useTodos.ts
@@ -1,13 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { MeteorApi } from 'meteor-type-validation';
 import { computed, reactive } from 'vue';
 import { TodosCollection } from '../../../api/todos/todos.collection';
 import { useSubscription } from '../useSubscription';
 import { useTracker } from '../useTracker';
+import 'meteor/meteor';
 
 export function useTodos() {
     const subscription = useSubscription('todos', {}, { limit: 50 })
     const todos = reactive({
         data: useTracker(() => TodosCollection.find({}).fetch()),
         ready: computed(() => subscription.ready),
+        async create() {
+            await MeteorApi.callAsync('todos.create', {
+                completed: false,
+                title: faker.lorem.sentence()
+            });
+        }
     });
     
     return todos;
