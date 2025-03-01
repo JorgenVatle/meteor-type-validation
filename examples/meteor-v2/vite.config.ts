86a0ed20f8b3dc26c9166bb39f8e7d9432cc3df7
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 04:07:24 +0200

    Correct Vite client entry path

--- examples/meteor-v2/vite.config.ts
+++ examples/meteor-v2/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         meteor({
-            clientEntry: 'client/entry-meteor.js',
+            clientEntry: 'client/entry-vite.ts',
             stubValidation: {
                 warnOnly: true,
             }
