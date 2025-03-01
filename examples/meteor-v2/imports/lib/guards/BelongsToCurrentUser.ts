c264d64e1819ce38e01f99013b6f0aaa9005a747
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 08:44:00 +0200

    Use patch strategy for BelongsToCurrentUser

--- examples/meteor-v2/imports/lib/guards/BelongsToCurrentUser.ts
+++ examples/meteor-v2/imports/lib/guards/BelongsToCurrentUser.ts
@@ -3,7 +3,7 @@ import * as v from 'valibot';
 
 export class BelongsToCurrentUser extends Guard {
     public readonly writeToContext = false;
-    public readonly writeToParams = true;
+    public readonly writeToParams = 'patch';
     
     public readonly contextSchema = UserAuthenticated.contextSchema;
     public readonly paramSchema = [
