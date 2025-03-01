86d6ff5e6e5e870955841c696caa448559aa9128
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 07:16:50 +0200

    Simplify main index module

--- packages/meteor-type-validation/src/index.ts
+++ packages/meteor-type-validation/src/index.ts
@@ -1,8 +1,7 @@
 import './types/meteor/ModuleAugmentation';
 
-export * from './server/guards';
-export * from './server/Definitions';
-export * from './server/MeteorTypeValidation';
-export * from './client/MeteorApi';
-
 export type * from './types/ValidatedResources';
+
+export * from './server';
+export * from './client';
+
