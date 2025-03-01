61be41b2619a77a14ecfbb4a126d4bbbccc2e299
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 07:12:35 +0200

    Move MeteorApi into client directory

--- packages/meteor-type-validation/src/index.ts
+++ packages/meteor-type-validation/src/index.ts
@@ -3,6 +3,6 @@ import './types/meteor/ModuleAugmentation';
 export * from './guards';
 export * from './Definitions';
 export * from './MeteorTypeValidation';
-export * from './MeteorApi';
+export * from './client/MeteorApi';
 
 export type * from './types/ValidatedResources';
