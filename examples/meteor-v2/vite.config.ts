a8b0ada0d9e25d57e3580861768a8fba6b163954
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 07:10:56 +0200

    Use file:// reference for meteor-type-validation

--- examples/meteor-v2/vite.config.ts
+++ examples/meteor-v2/vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue';
 import { meteor } from 'meteor-vite/plugin';
-import Path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -13,9 +12,4 @@ export default defineConfig({
         }),
         vue(),
     ],
-    resolve: {
-        alias: {
-            'meteor-type-validation': Path.join(__dirname, '..', '..', 'packages', 'meteor-type-validation', 'src', 'index.ts'),
-        }
-    }
 })
