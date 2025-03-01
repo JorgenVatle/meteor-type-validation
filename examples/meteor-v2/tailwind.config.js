fbc8a411cd700a4f67376a0de673f76874ef9379
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 04:08:38 +0200

    Update paths for content

--- examples/meteor-v2/tailwind.config.js
+++ examples/meteor-v2/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      'client/**/*.{ts,html}',
-      'imports/**/*.{ts,vue,css}',
+      './client/**/*.{ts,html}',
+      './imports/**/*.{ts,vue,css}',
   ],
   theme: {
     extend: {},
