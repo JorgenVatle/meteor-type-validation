eeccfcc98073df92fc9d27e5c2ee7c802116b2f8
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 11:57:43 +0200

    Correct guard reference for BelongsToCurrentUser.ts

--- examples/meteor-v2/imports/lib/guards/BelongsToCurrentUser.ts
+++ examples/meteor-v2/imports/lib/guards/BelongsToCurrentUser.ts
@@ -1,11 +1,11 @@
-import { Guard, UserAuthenticated } from 'meteor-type-validation';
+import { Guard, UserLoggedInGuard } from 'meteor-type-validation';
 import * as v from 'valibot';
 
 export class BelongsToCurrentUser extends Guard {
     public readonly writeToContext = false;
     public readonly writeToParams = 'patch';
     
-    public readonly contextSchema = UserAuthenticated.contextSchema;
+    public readonly contextSchema = UserLoggedInGuard.contextSchema;
     public readonly paramSchema = [
         v.pipe(
             v.object({}),
