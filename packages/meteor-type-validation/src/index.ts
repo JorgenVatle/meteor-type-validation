a99f2e640c28c8488602c809947d96e6306fb002
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 05:37:41 +0200

    Re-export MeteorApi utility

--- packages/meteor-type-validation/src/index.ts
+++ packages/meteor-type-validation/src/index.ts
@@ -3,5 +3,6 @@ import './types/meteor/ModuleAugmentation';
 export * from './guards';
 export * from './Definitions';
 export * from './MeteorTypeValidation';
+export * from './MeteorApi';
 
 export type * from './types/ValidatedResources';
