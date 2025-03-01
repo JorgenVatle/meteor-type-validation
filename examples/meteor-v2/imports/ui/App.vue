cf5f8f85f816ee87442e4643269bc5672ba457b6
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-01 04:27:30 +0200

    Add button click example

--- examples/meteor-v2/imports/ui/App.vue
+++ examples/meteor-v2/imports/ui/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1>
-    Hello Vue!
-  </h1>
+  <div class="container mx-auto">
+    <div class="grid gap-8">
+      <div>
+        <h1 class="text-2xl font-bold">
+          Hello Vue!
+        </h1>
+      </div>
+      <div>
+        <button @click="count++" class="btn">This button has been clicked {{ count }} times</button>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 
+const count = ref(0);
 </script>
