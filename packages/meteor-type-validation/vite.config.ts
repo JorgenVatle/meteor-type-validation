135d8da18435619e4788cdbec6d3d60467cc22fb
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-29 14:50:43 +0200

    Add Meteor stub for running Vitest

--- packages/meteor-type-validation/vite.config.ts
+++ packages/meteor-type-validation/vite.config.ts
@@ -5,6 +5,21 @@ import viteTsConfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
     plugins: [
         viteTsConfigPaths(),
+        {
+            name: 'meteor-import-stubs',
+            resolveId(id) {
+                if (id.startsWith('meteor/')) {
+                    return id;
+                }
+            },
+            load(id) {
+                if (id.startsWith('meteor/meteor')) {
+                    // This should probably be refactored if we build with Vite instead of tsup
+                    // Currently serves as stubs for the test environment.
+                    return `export const Meteor = globalThis.Meteor || { Error: Error }`;
+                }
+            }
+        }
     ],
     test: {
         dir: 'tests',
