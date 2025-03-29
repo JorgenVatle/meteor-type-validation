ce919e85cbed1b842c953a8cd3639f4e8258d3d9
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-29 11:05:45 +0200

    Use helper types for publication and subscribe handles

--- packages/meteor-type-validation/src/types/meteor/ModuleAugmentation.ts
+++ packages/meteor-type-validation/src/types/meteor/ModuleAugmentation.ts
@@ -6,29 +6,24 @@ declare module 'meteor/meteor' {
     export type PublicationName = keyof DefinedPublications;
     export type PublicationParams<TName extends PublicationName> = Parameters<DefinedPublications[TName]>;
     export type MethodParams<TName extends MethodName> = Parameters<DefinedMethods[TName]>;
-    export type MethodResult<TName extends MethodName> = ReturnType<DefinedMethods[TName]>;
+    export type MethodResult<TName extends MethodName> = Awaited<ReturnType<DefinedMethods[TName]>>;
     export type PublicationResult<TName extends PublicationName> = ReturnType<DefinedPublications[TName]>;
     
     namespace Meteor {
         function subscribe<
-            TName extends keyof DefinedPublications
+            TName extends PublicationName
+        >(name: TName, ...params: PublicationParams<TName>): Meteor.SubscriptionHandle;
+        
+        function call<
+            TName extends MethodName
         >(
             name: TName,
-            ...params: Parameters<DefinedPublications[TName]>
-        ): Meteor.SubscriptionHandle;
-        
-        function call<TName extends keyof DefinedMethods>(
-            name: TName,
-            ...params: [
-                ...Parameters<DefinedMethods[TName]>,
-                callback?: (error?: Error, response?: ReturnType<DefinedMethods[TName]>) => void
-            ],
+            ...params: [...MethodParams<TName>, callback?: (error?: Error, response?: MethodResult<TName>) => void],
         ): void;
         
-        function callAsync<TName extends keyof DefinedMethods>(
-            name: TName,
-            ...params: Parameters<DefinedMethods[TName]>
-        ): Awaited<ReturnType<DefinedMethods[TName]>>;
+        function callAsync<
+            TName extends MethodName
+        >(name: TName, ...params: MethodParams<TName>): Promise<MethodResult<TName>>;
     }
 }
 
