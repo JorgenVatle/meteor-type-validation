796b1cd7ec5dce4ac5c73511d9683f71b17a8cdb
Jørgen Vatle <jorgen@wcaservices.net>
2025-03-29 12:37:09 +0200

    Add path alias for utils

--- packages/meteor-type-validation/tests/Humanize.test.ts
+++ packages/meteor-type-validation/tests/Humanize.test.ts
@@ -1,5 +1,5 @@
+import { humanizeProperty } from '@meteor-type-validation/utils';
 import { expect, it } from 'vitest';
-import { humanizeProperty } from '../src/utils/Humanize';
 
 it('should split camelCase', () => {
     expect(humanizeProperty('createdBy')).toBe('Created by');
