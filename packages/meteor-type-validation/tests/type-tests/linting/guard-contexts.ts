4982ed5775c123e4c1e22ea1e70049a34cf7d708
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-08 20:55:35 +0300

    Add missing guard class to guard context linting tests

--- packages/meteor-type-validation/tests/type-tests/linting/guard-contexts.ts
+++ packages/meteor-type-validation/tests/type-tests/linting/guard-contexts.ts
@@ -2,11 +2,31 @@ import {
     defineMethods,
     definePublications,
     exposeMethods,
-    exposePublications,
+    exposePublications, Guard,
     UserLoggedInGuard,
 } from '@meteor-type-validation/server';
+import { Meteor } from 'meteor/meteor';
 import * as v from 'valibot';
-import { CreatedByCurrentUser } from '../../lib/CreatedByCurrentUserGuard';
+
+class CreatedByCurrentUser extends Guard {
+    public readonly writeToParams = false;
+    public readonly writeToContext = false;
+    
+    public readonly contextSchema = UserLoggedInGuard.contextSchema;
+    
+    public readonly paramSchema = [
+        v.object({
+            createdBy: v.string(),
+        }),
+    ]
+    
+    public validate() {
+        this.assertContext();
+        if (this.params[0].createdBy !== this.context.userId) {
+            throw new Meteor.Error(401, 'You do not have permission for this resource');
+        }
+    }
+}
 
 export const AllMethods = defineMethods({
     'user:todo.add': {
