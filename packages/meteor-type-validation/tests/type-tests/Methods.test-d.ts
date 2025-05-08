4589ede7a86d0008d3224739ec8ec109a2cab2a6
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-08 18:58:50 +0300

    Correct syntax error

--- packages/meteor-type-validation/tests/type-tests/Methods.test-d.ts
+++ packages/meteor-type-validation/tests/type-tests/Methods.test-d.ts
@@ -69,7 +69,8 @@ describe('larger method objects', () => {
                     expectTypeOf(entry).toEqualTypeOf<{
                         newUserId: string;
                     }>();
+                }
             }
-        })
+        });
     })
 })
